3869baa12d488125e36706f1a5153a0a0ab26411
vaibhav7766 <vaibhavsharmav777@gmail.com>
2023-11-11 23:18:21 +0530

    new file:   best_divisor.py

---
+++ best_divisor.py
@@ -0,0 +1,32 @@
+#https://www.hackerrank.com/challenges/best-divisor/problem?isFullScreen=true
+
+import sys
+input = sys.stdin.readline
+
+def get_divisors(n):
+    div = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            div.append(i)
+    return div
+
+def digit_sum(n):
+    s = str(n)
+    sn = sum(map(lambda x: int(x), s))
+    return sn
+
+def best(n):
+    divisors = get_divisors(n)
+    d = {}
+    temp = 0
+    max_sum = 0
+    for i in divisors:
+        sn = digit_sum(i)
+        d[i] = sn
+        if max_sum < sn:
+            max_sum = sn
+            temp = i
+    return temp
+
+n = int(input())
+print(best(n))
